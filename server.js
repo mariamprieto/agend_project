2dc8432cab6af51d0462b2c4b9424f9971f6444c
mariamprieto <mariaprieto@byui.edu>
2022-07-28 14:13:42 -0400

    update

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+const myconn=require('express-myconnection')
+
+
 app.set('port', process.env.PORT || 9000)
 
+const dbOptions = {
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'abc123',
+    database: 'escola'
+}
+//middlewares--------------------------
+app.use(myconn(mysql,dbOptions, 'single'))
+
+//routes--------------------------------
 app.get('/', (req, res) => {
     res.send('working')
 })
 
-
+//server running-------------------------
 app.listen(app.get ('port'), () => {
     console.log('server running on port',app.get('port'))
 })
